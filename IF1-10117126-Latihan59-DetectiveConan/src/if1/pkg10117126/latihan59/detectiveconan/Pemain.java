a91360f60cf8bef1ffe3406c8e7e2531e868c609
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-11-20 17:30:06 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan59-DetectiveConan/src/if1/pkg10117126/latihan59/detectiveconan/Pemain.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117126.latihan59.detectiveconan;
+
+/**
+ *
+ * @author MAWA
+ */
+public class Pemain {
+
+    private String nama;
+    private String peran;
+
+    public Pemain(String nama, String peran) {
+        this.nama = nama;
+        this.peran = peran;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getPeran() {
+        return peran;
+    }
+}
